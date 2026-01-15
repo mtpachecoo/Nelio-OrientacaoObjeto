e777eaeb0486d921b943c83f43a64b47eecf2f26
Matheus Pacheco <155036560+mtpachecoo@users.noreply.github.com>
2026-01-15 20:39:57 +0000

    Praticando conceito de Vetores Secao 6

---
+++ secao6/Program.cs
@@ -0,0 +1,16 @@
+﻿
+System.Console.WriteLine("Qual será o tamanho da lista?");
+int n = int.Parse(Console.ReadLine());
+double[] vect = new double[n];
+
+System.Console.WriteLine("Digite os valores da lista");
+for(int i = 0; i < n; i++)
+{
+    vect[i] = double.Parse(Console.ReadLine());
+}
+
+foreach (double list in vect)
+{
+    System.Console.WriteLine(list);
+}
+
