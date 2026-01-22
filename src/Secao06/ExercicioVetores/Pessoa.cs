dae8f76bf37bd30d0fc69ef8e89ffb0e85e1ac27
Matheus Pacheco <155036560+mtpachecoo@users.noreply.github.com>
2026-01-22 18:38:31 +0000

    FEAT: ADICIONA REGRAS DE NEGOCIO, e ArgumentException

--- src/Secao06/ExercicioVetores/Pessoa.cs
+++ src/Secao06/ExercicioVetores/Pessoa.cs
@@ -1,14 +1,34 @@
 public class Pessoa
 {
-    public string Nome { get; set; }
-    public string Email { get; set; }
-
-    public int Quarto { get; set; }
+    public string Nome { get; init; }
+    public string Email { get; init; }
+    public int Quarto { get; private set; }
     public Pessoa(string nome, string email, int quarto)
     {
+        if(email.Contains("@"))
+        {
+            throw new ArgumentException("Email incorreto");
+        }
+        if (string.IsNullOrWhiteSpace(nome))
+        {
+            throw new ArgumentException ("Nome não deve ser vazio");
+        }
+        if (quarto < 0 && quarto > 9)
+        {
+            throw new ArgumentException ("Quarto disponivel entre 0 e 9! ");
+        }
         Nome = nome;
-        Email = email;
         Quarto = quarto;
+        Email = email;
+    }
+
+    public void AlterarQuarto(int novoNumero, bool quartoVazio)
+    {
+        if (!quartoVazio)
+            throw new ArgumentException ("Quarto reservado!");
+        
+        Quarto = novoNumero;
+        
     }
 
     public override string ToString()
