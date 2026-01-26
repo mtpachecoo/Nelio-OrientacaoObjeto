283e0f74b539a6ccd2939fb0727748d6d244c468
Matheus Pacheco <155036560+mtpachecoo@users.noreply.github.com>
2026-01-26 19:58:08 +0000

    feat: add matrix exercise with negative count and main diagonal logic

---
+++ src/Secao06/ExercicioMatriz/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ExercicioMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número de linhas e colunas da matriz: ");
+            int linhasColunas = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[linhasColunas,linhasColunas];
+            for(int i = 0; i < linhasColunas; i++)
+            {
+                System.Console.WriteLine($"Digite os elementos da linha {i+1}");
+                string [] values = Console.ReadLine().Split(' ');
+
+                for(int j = 0; j < linhasColunas; j++)
+                {
+                    matriz[i,j] = int.Parse(values[j]);
+                }
+            }
+            System.Console.WriteLine("Main diagonal:");
+            for ( int i = 0; i < linhasColunas; i++)
+            {
+                System.Console.Write(matriz[i, i] + " ");
+            }
+        
+            int count = 0;
+            for(int i = 0; i<linhasColunas; i++)
+            {
+                for(int j = 0; j < linhasColunas; j++)
+                {
+                    if (matriz[i,j] < 0) {
+                        count ++;
+                    }
+                }
+            }
+            System.Console.WriteLine($"Negative numbers {count}");
+
+           
+        }
+                
+    }    
+}
